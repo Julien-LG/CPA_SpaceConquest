2091779d03f80b800ba6c47987473df5decc5b3e
Julien LG <58564546+Julien-LG@users.noreply.github.com>
2024-04-11 11:03:31 +0200

    tests pixijs

---
+++ pixiJS tests/main.js
@@ -0,0 +1,3 @@
+const app = new PIXI.Application();
+await app.init({ width: 640, height: 360 });
+document.body.appendChild(app.canvas);
---
+++ pixiJS tests/module/main.js
@@ -0,0 +1,5 @@
+// main.js
+import { add, subtract } from './math.mjs';
+
+console.log(add(5, 3)); // Output: 8
+console.log(subtract(10, 4)); // Output: 6
