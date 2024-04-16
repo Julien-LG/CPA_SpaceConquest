db56a5307b613c1a40a43f408104e27e4f74cca3
Julien LG <58564546+Julien-LG@users.noreply.github.com>
2024-04-16 14:46:12 +0200

    clean

--- react/Circle.js
+++
@@ -1,9 +0,0 @@
-// Circle.js
-import React from 'react';
-import './Shapes.css'; // Styles for shapes
-
-const Circle = () => {
-  return <div className="circle"></div>;
-}
-
-export default Circle;
