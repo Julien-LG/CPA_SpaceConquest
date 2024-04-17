962def78b326a26ab93737e014479021a33ab918
Ewenou <ewen.glasziou@gmail.com>
2024-04-17 16:23:22 +0200

    Update ai.ts

--- Version_fonctionnelle/src/components/Canvas/Model/ai.ts
+++ Version_fonctionnelle/src/components/Canvas/Model/ai.ts
@@ -1,42 +1,72 @@
-import { OurModel, Circle, Triangle, setDestinationEnemy } from './model';
+import { OurModel, Circle, Point, setDestinationEnemy } from './model';
 import * as conf from '../conf';
 
 // Calcul la distance entre deux points, on ne prend pas la racine carrée pour éviter de faire une opération coûteuse (les comparaison de distance restent valides)
 const calculateDistance = (x1: number, y1: number, x2: number, y2: number): number => {
     return ((x2 - x1) ** 2 + (y2 - y1) ** 2);
 };
+const calculateCentroid = (planets: Circle[]): Point => {
+    const total = planets.reduce((acc, planet) => {
+        acc.x += planet.center.x;
+        acc.y += planet.center.y;
+        return acc;
+    }, { x: 0, y: 0 });
 
+    return {
+        x: total.x / planets.length,
+        y: total.y / planets.length
+    };
+};
+
+/**************************************************
+ * Fonctions pour les IA (Pas très complexe)
+ *************************************************/
 // Attaque la planète la plus proche 
 export const directTrianglesToNearestPlanet = (model: OurModel, enemyColor: string): OurModel => {
-    model.circles.forEach(circle => {
-        if (circle.color === enemyColor) {
-            const target = model.circles
-                .filter(c => c.color !== enemyColor)
-                .reduce((prev, curr) => calculateDistance(circle.center.x, circle.center.y, prev.center.x, prev.center.y) < calculateDistance(circle.center.x, circle.center.y, curr.center.x, curr.center.y) ? prev : curr);
+    const enemyPlanets = model.circles.filter(circle => circle.color === enemyColor);
 
-            const destination = { x: target.center.x, y: target.center.y };
-            model = setDestinationEnemy(model, destination, enemyColor);
-        }
+    if (enemyPlanets.length === 0) {
+        return model; // No enemy planets to calculate centroid
+    }
+
+    const centroid = calculateCentroid(enemyPlanets);
+
+    const nearestPlanet = model.circles
+        .filter(circle => circle.color !== enemyColor)
+        .reduce((closest, current) => {
+            const closestDistance = calculateDistance(centroid.x, centroid.y, closest.center.x, closest.center.y);
+            const currentDistance = calculateDistance(centroid.x, centroid.y, current.center.x, current.center.y);
+            return currentDistance < closestDistance ? current : closest;
     });
+    const destination = { x: nearestPlanet.center.x, y: nearestPlanet.center.y };
+    model = setDestinationEnemy(model, destination, enemyColor);
+    
     return model;
 };
 
 // Attaque la planète ennemie la plus faible et la plus proche
 export const directTrianglesToWeakestClosestEnemy = (model: OurModel, enemyColor: string): OurModel => {
-    model.circles.forEach(circle => {
-        if (circle.color === enemyColor) {
-            const target = model.circles
-                .filter(c => c.color !== enemyColor && c.color !== conf.UNHABITEDPLANETCOLOR)
-                .sort((a, b) => {
-                    const distA = calculateDistance(circle.center.x, circle.center.y, a.center.x, a.center.y);
-                    const distB = calculateDistance(circle.center.x, circle.center.y, b.center.x, b.center.y);
-                    return distA !== distB ? distA - distB : a.hp - b.hp;
-                })[0];
+    const enemyPlanets = model.circles.filter(circle => circle.color === enemyColor);
 
-           
-            const destination = { x: target.center.x, y: target.center.y };
-            model = setDestinationEnemy(model, destination, enemyColor);
-        }
-    });
+    if (enemyPlanets.length === 0) {
+        return model; // No enemy planets to calculate centroid
+    }
+
+    const centroid = calculateCentroid(enemyPlanets);
+
+    const nearestAndWeakestHabitedPlanet = model.circles.filter(circle => circle.color !== enemyColor && circle.color !== conf.UNHABITEDPLANETCOLOR)
+        .reduce((closest, current) => {
+            const closestDistance = calculateDistance(centroid.x, centroid.y, closest.center.x, closest.center.y);
+            const currentDistance = calculateDistance(centroid.x, centroid.y, current.center.x, current.center.y);
+            if (currentDistance < closestDistance) {
+                return current;
+            } else if (currentDistance === closestDistance) {
+                return closest.hp < current.hp ? closest : current;
+            }
+            return closest;
+        });
+    const destination = { x: nearestAndWeakestHabitedPlanet.center.x, y: nearestAndWeakestHabitedPlanet.center.y };
+    model = setDestinationEnemy(model, destination, enemyColor);
+    
     return model;
 };
